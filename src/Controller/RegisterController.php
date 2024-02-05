dfd8ead63bc1bfd7f6aa257b1e6a16d8d56050f0
Bontano <hassen.tom07@gmail.com>
2024-02-05 15:43:49 +0100

    maj

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,14 +4,16 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterController extends AbstractController
 {
 
-    public function __invoke(Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $em)
+    public function __invoke(Request $request, UserPasswordHasherInterface $hasher, JWTTokenManagerInterface $JWTTokenManager,EntityManagerInterface $em)
     {
         $data = json_decode($request->getContent());
         $user = new User();
@@ -21,8 +23,7 @@ class RegisterController extends AbstractController
         $user->setRoles(['ROLE_USER']);
         $em->persist($user);
         $em->flush();
-dd($user);
-        return $user;
+        return new Response($JWTTokenManager->create($user), 200);
     }
 
 

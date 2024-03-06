e29db2e73ec7db3eb74fdfbe0e84ea5907390517
unknown <mannoni.pa@gmail.com>
2024-03-06 08:08:09 +0100

    ajout d'un commentaire

--- src/Controller/PostItinaryController.php
+++ src/Controller/PostItinaryController.php
@@ -32,7 +32,10 @@ class PostItinaryController extends AbstractController
         ->ville->semaine->jour->moment de la journée.
         Je veux que tu me retourne uniquement le json sans texte superflu";
         $prompt = $firstPromptSubstring . " " . $city . " " . $secondPromptSubstring . " " . $startDate . " " . $thirdPromptSubstring . " " . $endDate . " " . $fourthPromptSubstring;
+        // Ligne pour utiliser le mock
         $itinary = $itinaryHandler->genererItineraireMock($prompt, $user);
+        // Ligne pour utiliser chatgpt
+        // $itinary = $itinaryHandler->genererItineraireOpenAi($prompt, $user);
         $json = $serializer->normalize(
             $itinary,
             null,
